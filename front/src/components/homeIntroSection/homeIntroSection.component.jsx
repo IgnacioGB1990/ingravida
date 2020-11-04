7dc9970f34feb37d0d53a942241bc152606cbe83
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-11-04 14:10:13 +0100

    Home Intro Updates

--- front/src/components/homeIntroSection/homeIntroSection.component.jsx
+++ front/src/components/homeIntroSection/homeIntroSection.component.jsx
@@ -4,10 +4,10 @@ import "./homeIntroSection.styles.css"
 export const HomeIntroSection = () => {
   return (
     <div className="Home-Intro-Section-Container">
-      <h1 className="Home-Intro-Section-Text">tu transformación empieza aquí</h1>
-      <h4 className="Home-Intro-Section-Logo">Cualesquiera que sean tu forma y tu textura, estamos aquí para ayudarte.</h4>
+      <h1 className="Home-Intro-Section-Text">saca a relucir tus rizos de una vez</h1>
+      <h4 className="Home-Intro-Section-Logo">Ya sea ondulazo, rizado o extra-rizado, tenemos el producto para ti.</h4>
       {/* <h3 className="Home-Intro-Button-Container"><a className="abcButton" href="/abc">Saber más</a></h3> */}
-      <a className="abcButton" href="/abc">Saber más</a>
+      <a className="abcButton" href="/abc">Saber Más</a>
     </div>
   )
 }
--- front/src/components/homeTest/homeTest.component.jsx
+++ front/src/components/homeTest/homeTest.component.jsx
@@ -4,9 +4,19 @@ import "./homeTest.styles.css"
 
 export const HomeTest = () => {
   return (
-    <div className="image-container">
-      <img className="image-fit" src={YolsPortada} height="200" alt="Home Page" />
-      <div className="centered-text">Test 2222222</div>
+    <div className="Home-Test-Container">
+
+      <div className="Home-Test-Section">
+        <div className="Home-Test-Section-Left">
+
+          <h2 className="Left-Header">responde a nuestro cuestionario sobre rizos</h2>
+          <h4 className="Left-Description">¿No estás segura de cuáles son los mejores productos para ti?
+          Encuentra tu rutina personalizada perfecta para ti gracias a
+             nuestro cuestionario.</h4>
+          <a className="testButton" href="/test">Responde a nuestro cuestionario</a>
+        </div>
+        <div className="Home-Test-Section-Right"></div>
+      </div>
     </div>
   )
 }
