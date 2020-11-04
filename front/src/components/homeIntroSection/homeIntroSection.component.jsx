b5ee9fced7e9a9c17c2cdc0dc992511c8aa44c47
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-11-04 11:25:41 +0100

    First Home Page Section Completed

--- front/src/components/homeIntroSection/homeIntroSection.component.jsx
+++ front/src/components/homeIntroSection/homeIntroSection.component.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 import "./homeIntroSection.styles.css"
-import { Button } from "../Navbar/Button"
 
 export const HomeIntroSection = () => {
   return (
     <div className="Home-Intro-Section-Container">
       <h1 className="Home-Intro-Section-Text">tu transformación empieza aquí</h1>
       <h4 className="Home-Intro-Section-Logo">Cualesquiera que sean tu forma y tu textura, estamos aquí para ayudarte.</h4>
-      <Button >Inicio</Button>
-
+      {/* <h3 className="Home-Intro-Button-Container"><a className="abcButton" href="/abc">Saber más</a></h3> */}
+      <a className="abcButton" href="/abc">Saber más</a>
     </div>
   )
 }
