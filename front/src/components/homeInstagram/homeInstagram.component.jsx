be896f9d07cc649190feee158976589feaebb623
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-12-02 17:18:59 +0100

    Home insta section update 2

--- front/src/components/homeInstagram/homeInstagram.component.jsx
+++ front/src/components/homeInstagram/homeInstagram.component.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import { instagramPictures } from "./homeInstagramPictures"
 import "./homeInstagram.styles.css"
+
 
 const HomeInstagram = () => {
   return (
     <div className="ContainerInstagram">
+      <h2 className="HeaderInstagram">Comparte tu viaje de rizos en #ingravida</h2>
+      <div className="ContainerImages">
+        {instagramPictures.map((picture, index) => {
+          return (
+            <ul className="InstaImagesContainer" key={index}>
+              <img className="InstaImages" src={picture.url}></img>
+              {picture.name}
+            </ul>
+          )
+        })}
 
-      Comparte tu viaje de rizos en #ingravida
 
+
+      </div>
     </div>
   )
 }
