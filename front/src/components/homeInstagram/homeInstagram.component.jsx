0575cc732376bb0e8f4141cd03d05d871b9c5c19
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-12-06 13:23:46 +0100

    Added option of opening instagram account on click

--- front/src/components/homeInstagram/homeInstagram.component.jsx
+++ front/src/components/homeInstagram/homeInstagram.component.jsx
@@ -4,21 +4,24 @@ import "./homeInstagram.styles.css"
 
 
 const HomeInstagram = () => {
+
+  const openUrl = (instapage) => {
+    // <a href={`https://www.instagram.com/${instapage}`} ></a >
+    console.log("hola")
+  }
+
+
+
   return (
     <div className="ContainerInstagram">
       <h2 className="HeaderInstagram">Comparte tu viaje de rizos en #ingravida</h2>
+
       <div className="ContainerImages">
         {instagramPictures.map((picture, index) => {
           return (
-            <ul className="InstaImagesContainer" key={index}>
-              <img className="InstaImages" src={picture.url}></img>
-              {picture.name}
-            </ul>
+            <img onClick={() => window.open(`https://www.instagram.com/${picture.instagram}`, "_blank")} key={index} className="InstaImages" src={picture.url}></img>
           )
         })}
-
-
-
       </div>
     </div>
   )
--- front/src/components/homeInstagram/homeInstagramPictures.js
+++ front/src/components/homeInstagram/homeInstagramPictures.js
@@ -6,6 +6,24 @@ export const instagramPictures = [
   },
 
   {
+    instagram: "yolandastems",
+    url: "https://res.cloudinary.com/djkhnzte2/image/upload/v1603626535/_IGB4473_rqjllf.jpg",
+    cName: ""
+  },
+
+  {
+    instagram: "yolandastems",
+    url: "https://res.cloudinary.com/djkhnzte2/image/upload/v1604572658/_IGB9272_rrevs4.jpg",
+    cName: ""
+  },
+
+  {
+    instagram: "torcuatofotos",
+    url: "https://res.cloudinary.com/djkhnzte2/image/upload/v1604147000/_IGB2881_as1gfn.jpg",
+    cName: ""
+  },
+
+  {
     instagram: "torcuatofotos",
     url: "https://res.cloudinary.com/djkhnzte2/image/upload/v1603626535/_IGB4473_rqjllf.jpg",
     cName: ""
@@ -33,7 +51,44 @@ export const instagramPictures = [
     instagram: "torcuatofotos",
     url: "https://res.cloudinary.com/djkhnzte2/image/upload/v1604572658/_IGB9272_rrevs4.jpg",
     cName: ""
+  },
+
+  {
+    instagram: "torcuatofotos",
+    url: "https://res.cloudinary.com/djkhnzte2/image/upload/v1604147000/_IGB2881_as1gfn.jpg",
+    cName: ""
+  },
+
+  {
+    instagram: "torcuatofotos",
+    url: "https://res.cloudinary.com/djkhnzte2/image/upload/v1603626535/_IGB4473_rqjllf.jpg",
+    cName: ""
+  },
+
+  {
+    instagram: "torcuatofotos",
+    url: "https://res.cloudinary.com/djkhnzte2/image/upload/v1604572658/_IGB9272_rrevs4.jpg",
+    cName: ""
+  },
+
+  {
+    instagram: "torcuatofotos",
+    url: "https://res.cloudinary.com/djkhnzte2/image/upload/v1604572658/_IGB9272_rrevs4.jpg",
+    cName: ""
+  },
+
+  {
+    instagram: "torcuatofotos",
+    url: "https://res.cloudinary.com/djkhnzte2/image/upload/v1604147000/_IGB2881_as1gfn.jpg",
+    cName: ""
+  },
+
+  {
+    instagram: "torcuatofotos",
+    url: "https://res.cloudinary.com/djkhnzte2/image/upload/v1603626535/_IGB4473_rqjllf.jpg",
+    cName: ""
   }
+
 
 
 
