89258daceb5552bdf7d3f3bf9b9f4d1b7ab2f269
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-12-13 13:14:42 +0100

    Messing around with react Carousel

--- front/src/components/homeInstagram/homeInstagram.component.jsx
+++ front/src/components/homeInstagram/homeInstagram.component.jsx
@@ -7,6 +7,15 @@ import "./homeInstagram.styles.css"
 
 const HomeInstagram = () => {
 
+  // const mystyle = {
+  //   width: "200px",
+  //   height: "150px",
+  //   marginleft: "auto",
+  //   marginright: "auto",
+  //   transition: "transform .2s",
+  // };
+
+
 
   return (
     // <div className="ContainerInstagram">
@@ -22,11 +31,18 @@ const HomeInstagram = () => {
     // </div>
     <div className="ContainerBackground">
       <h2 className="HeaderInstagram">Comparte tu viaje de rizos en #ingravida</h2>
-      <Carousel className="carouselContainer">
-        <Carousel.Item className="ContainerImages">
+      <Carousel interval={false} >
+        <Carousel.Item >
           {instagramPictures.map((picture, index) => {
             return (
-              <img onClick={() => window.open(`https://www.instagram.com/${picture.instagram}`, "_blank")} key={index} className="InstaImages" src={picture.url}></img>
+              <img onClick={() =>
+                window.open(`https://www.instagram.com/${picture.instagram}`, "_blank")}
+                key={index}
+                className="InstaImages"
+                // style={mystyle}
+                // className="d-block w-100"
+                src={picture.url}
+              ></img>
             )
           })}
         </Carousel.Item>
