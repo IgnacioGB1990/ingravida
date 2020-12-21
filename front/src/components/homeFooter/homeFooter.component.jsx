f9d61ef6f2b117116ff6c948c31ff57b9bd7b72b
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-12-21 13:37:11 +0100

    Added social media icons on footer with font awesome

--- front/src/components/homeFooter/homeFooter.component.jsx
+++ front/src/components/homeFooter/homeFooter.component.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
 import "./homeFooter.styles.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faYoutube, faFacebook, faTwitter, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 const HomeFooter = () => {
   return (
     <div className="ContainerFooter">
 
-      This is the footer baby
+
+      <div className="social-container">
+        <a href="www.youtube.com "
+          target="_blank"
+          className="youtube social"
+        >
+          <FontAwesomeIcon icon={faYoutube} size="2x" />
+        </a>
+
+        <a href="www.facebook.com "
+          target="_blank"
+          className="facebook social"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+
+        <a href="www.youtube.com "
+          target="_blank"
+          className="twitter social"
+        >
+          <FontAwesomeIcon icon={faTwitter} size="2x" />
+        </a>
+
+        <a href="www.instagram.com "
+          target="_blank"
+          className="instagram social"
+        >
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+      </div>
 
     </div>
   )
