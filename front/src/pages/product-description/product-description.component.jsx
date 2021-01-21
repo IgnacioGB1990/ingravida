1b060ee1ffbe51c8826e57c4fb428d71ef28d307
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2021-01-21 21:46:38 +0100

    Working onclick product call product description component

--- front/src/pages/product-description/product-description.component.jsx
+++ front/src/pages/product-description/product-description.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import "./product-description.styles.scss"
+import ProductDescription from "../../components/product-description/product-description.component"
+
 
 function ProductDescriptionPage() {
   return (
@@ -8,7 +10,7 @@ function ProductDescriptionPage() {
     <div className="product-container" >
 
       PRODUCT DESCRIPTION
-      
+      <ProductDescription />
 
 
     </div>
