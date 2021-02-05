068c8f9ce9aed61d4eedcecbd3b22a4f91ac0901
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2021-02-05 17:32:01 +0100

    Added spinner component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,11 +4,20 @@ import { connect } from "react-redux";
 
 import CollectionsOverview from "../../components/shop/collections-overview/collections-overview.component"
 import ProductDescriptionPage from "../product-description/product-description.component"
+import WithSpinner from "../../components/with-spinner/with-spinner.component"
+
 import { updateCollections } from "../../redux/shop/shop.actions"
 import { firestore, convertCollectionsSnapshotToMap } from "../../components/signInUp/firebase/firebase.utils"
 import "./shop.styles.css"
 
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview)
+
 class ShopPage extends React.Component {
+
+  state = {
+    loading: true
+  }
 
   unSubscribeFromSnapshot = null;
 
@@ -16,12 +25,10 @@ class ShopPage extends React.Component {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
 
-
-
-    collectionRef.onSnapshot(async snapshot => {
+    this.unSubscribeFromSnapshot = collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshot)
       updateCollections(collectionsMap)
-
+      this.setState({ loading: false })
     })
 
   }
@@ -29,9 +36,10 @@ class ShopPage extends React.Component {
   render() {
 
     const { match } = this.props
+    const { loading } = this.state;
     return (
       <div className="shop-page">
-        < Route exact path={`${match.path}`} component={CollectionsOverview} />
+        < Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
         < Route path={`${match.path}/:productId`} component={ProductDescriptionPage} />
       </div>
     );
@@ -46,3 +54,7 @@ const mapDispatchToProps = dispatch => ({
 
 
 export default connect(null, mapDispatchToProps)(ShopPage);
+
+
+
+{/* < Route exact path={`${match.path}`} component={CollectionsOverview}  /> */ }
